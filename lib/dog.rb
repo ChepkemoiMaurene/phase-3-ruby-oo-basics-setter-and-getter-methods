a7c38718f709c29764d1802f31e9414fd00b09f3
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-10-22 08:55:38 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
-
+    attr_reader:name
+    attr_reader:breed
+    attr_writer:name
+    attr_writer:breed
+    def name_setter (name,breed)
+        @name=name
+        @breed=breed
+    end
 end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
+    attr_reader:job
+    attr_writer:job
+    attr_writer:name
 
+    def name (name,job)
+        @name=name
+        @job=job
+
+    end
+    def name
+        @name
+    end
 end
